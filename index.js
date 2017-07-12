a43910c5921d037b8135a4689f36edd853db12ec
Tony Luisi <t.luisi@gmail.com>
2017-07-13 09:54:29 +1200

    changed port to PORT, and nodemon to node

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var server = require('./server')
 
-var PORT = process.env.port || 3000
+var PORT = process.env.PORT || 3000
 
 server.listen(PORT, function () {
   console.log('Server is listening on port', PORT)
