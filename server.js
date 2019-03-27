ca3892d6eb7bb0c9da30f29de9a71d74b66170aa
Bryce Kehoe <bryce@kehoe.co.nz>
2019-03-27 17:00:23 +1300

    Update var to const

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require('express')
-var hbs = require('express-handlebars')
+const express = require('express')
+const hbs = require('express-handlebars')
 
-var routes = require('./routes')
+const routes = require('./routes')
 
-var app = express()
+const app = express()
 
 // Middleware
 app.engine('hbs', hbs({
