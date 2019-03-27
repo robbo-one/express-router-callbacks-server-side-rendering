7db0efc723bb3314c8f4a0b2888aa44aa4172e87
Bryce Kehoe <bryce@kehoe.co.nz>
2019-03-27 17:00:35 +1300

    Rename app to server

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const hbs = require('express-handlebars')
 
 const routes = require('./routes')
 
-const app = express()
+const server = express()
 
 // Middleware
-app.engine('hbs', hbs({
+server.engine('hbs', hbs({
   defaultLayout: 'main',
   extname: 'hbs'
 }))
-app.set('view engine', 'hbs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: false }))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
 
 // Routes
-app.use('/', routes)
+server.use('/', routes)
 
-module.exports = app
+module.exports = server
