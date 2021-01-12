d32cd56cb00ea4f31c965b357209d2842f9b6f17
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-01-13 12:36:56 +1300

    completed just up to stretch

--- routes.js
+++ routes.js
@@ -30,4 +30,61 @@ router.get("/:id", (req, res) => {
 
 })
 
+router.get('/:id/edit', (req, res) => {
+    let viewData = {
+        id: 0,
+        name: "",
+        owner: "",
+        image : "",
+        breed: ""
+
+    }
+    fs.readFile('./data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log('There was an error', err)
+            } else {
+              let puppyInfo = {
+            ...JSON.parse(data)
+              }
+            viewData = puppyInfo.puppies.find(image => image.id == req.params.id)
+              res.render("edit", viewData)
+              return
+            }
+        })
+})
+
+
+router.post('/:id/edit', (req, res) => {
+    let viewData = {
+        id: 0,
+        name: "",
+        owner: "",
+        image : "",
+        breed: ""
+     }
+     fs.readFile('./data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log('There was an error', err)
+            } else {
+              let puppyInfo = {
+            ...JSON.parse(data)
+            }
+              viewData = puppyInfo.puppies.find(image => image.id == req.params.id)
+            //   JSON.stringify(viewData)
+              fs.writeFile("./data.json",JSON.stringify(viewData), (err) => {
+                if(err) {
+                    console.log("there is an error: " , err)
+                } else {
+                    res.redirect("../" + req.params.id)
+                }
+              })
+
+            //   res.redirect("../" + req.params.id)
+            }
+
+        })
+
+})
+
+
 module.exports = router
