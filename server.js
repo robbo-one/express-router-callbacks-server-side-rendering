4aac5d7e1db03096dec0824864bc2b9439b9f90a
student <student@eda-bc-11.localdomain>
2021-01-13 11:01:28 +1300

    puppies showing up

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const hbs = require('express-handlebars')
-
 const server = express()
-
+ const fs = require('fs')
+ const puppies = require('./data.json')
+// const routes = require('./routes.js')
 // Server configuration
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: false }))
@@ -12,6 +13,17 @@ server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
-
+server.get('/', (req,res) => {
+  fs.readFile('./data.json', 'utf-8', (err, data) => {
+    if (err){
+      console.log('puppies Not working')
+    }
+    else {
+     
+      data =JSON.parse(data)
+      res.render('home', data)
+    }
+    })
+  })
 
 module.exports = server
