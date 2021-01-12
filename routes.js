5ddac6babfa7ce4d5f10d7a4bf6359847578e283
Lara Hayward <22077219+haywardlara@users.noreply.github.com>
2021-01-13 11:44:01 +1300

    Can edit puppies.

--- routes.js
+++ routes.js
@@ -2,10 +2,29 @@ const express = require('express')
 const { route } = require('./server')
 const routes = express.Router()
 const puppies = require('./data.json')
+const fs = require('fs')
 module.exports = routes
 
-
-
+routes.get('/:id/edit', (req,res) => {
+  let id = req.params.id
+  let puppy = puppies.puppies.find(element => element.id == id)
+  res.render('edit', puppy)
+})
+routes.post('/:id/edit', (req,res) => {
+  let id = req.params.id
+  let puppy = puppies.puppies.find(element => element.id == id)
+  puppy.name = req.body.name
+  puppy.breed = req.body.breed
+  puppy.owner = req.body.owner
+  fs.writeFile('./data.json', JSON.stringify(puppies), (err) => {
+    if (err){
+      throw err;
+    }
+    console.log("Your edits have been saved.")
+  })
+  //console.log(puppies)
+  res.redirect(`/puppies/${id}`)
+})
 
 routes.get('/:id',(req, res) => {
   let id = req.params.id
