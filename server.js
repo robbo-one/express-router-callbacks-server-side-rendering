a88eca750038cacdf85b18ce530787fb7bed07ad
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-01-13 10:33:06 +1300

    Puppies rendering

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 
+const fs = require('fs')
+
 const server = express()
 
 // Server configuration
@@ -12,6 +14,18 @@ server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
-
+server.get('/', (req, res) => {
+  
+  fs.readFile('./data.json', (err, puppies) => {
+    if (err) {
+      console.log('Something went wrong: ', err)
+  } else { 
+    const parse = JSON.parse(puppies)
+    res.render('home', parse)
+    } 
+  })
+  
+    
+})
 
 module.exports = server
