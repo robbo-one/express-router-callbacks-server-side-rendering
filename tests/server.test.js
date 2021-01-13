f3f86248fef61cd0ea89866c0eb7f94639666545
jeff-seymour <73873192+jeff-seymour@users.noreply.github.com>
2021-01-13 15:21:20 +1300

    basic tests OK

---
+++ tests/server.test.js
@@ -0,0 +1,16 @@
+const server = require("../server")
+const request = require("supertest")
+const cheerio = require("cheerio")
+
+test ("tests are running", () => {
+  expect(2).toBeGreaterThan(1)
+})
+
+test ("/ exists", done => {
+  request(server)
+  .get("/")
+  .end((err, res) => {
+      expect(res.status).toBe(200)
+      done()
+  })
+})
